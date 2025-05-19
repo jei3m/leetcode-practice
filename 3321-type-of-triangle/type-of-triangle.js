6636057040f66908ff1a4011d35d2327948280c6
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-19 18:15:01 +0800

    Time: 1 ms (43.62%) | Memory: 54.4 MB (70.21%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function(nums) {
+    [a,b,c] = nums; 
+//xd
+    if (a + b > c && a + c > b && c + b > a ) {
+        if (a == b && b==c) {
+            return "equilateral"
+        } else if (a == b || b == c || a == c) {
+            return "isosceles"
+        } else {
+            return "scalene"
+        }
+    }
+    return "none"
+};
