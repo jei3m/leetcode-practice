d68d4f624201f80711ea89423953444c35e88337
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-19 22:26:33 +0800

    Time: 15 ms (13.91%) | Memory: 65 MB (40.22%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -2,13 +2,14 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function (x) {
-    const string = x.toString();
-    const splitted = string.split("")
-    const reverse = splitted.reverse()
-    const join = reverse.join("")
-    if (string === join ) {
-        return true;
-    } 
-    return false;
+var isPalindrome = function(x) {
+    let string = x.toString();
+    let split = string.split("");
+    let xString = split.join("");
+    let reverse = split.reverse().join("")
+    if (reverse === xString) {
+        return true
+    } else {
+        return false
+    }
 };
