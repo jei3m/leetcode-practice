dc58880becf948703095a3f3727612e06a9df38e
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-26 22:24:24 +0800

    Time: 63 ms (10.94%) | Memory: 54.8 MB (66.23%) - LeetSync

--- 2741-function-composition/function-composition.js
+++ 2741-function-composition/function-composition.js
@@ -5,8 +5,8 @@
 var compose = function(functions) {
     
     return function(x) {
-        for (const fn of functions.reverse()) {
-            x = fn(x)
+        for (let i = functions.length - 1; i >=0; i--) {
+            x = functions[i](x);
         }
         return x;
     }
