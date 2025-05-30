bcfde793bb4a3c55a9e2fff5f986fc27d2878fc0
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-31 05:37:56 +0800

    Time: 10 ms (17.27%) | Memory: 60.6 MB (19.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,11 +3,15 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let reversed = s.split(" ").join("").split("").reverse().join("").replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    let notReversed = s.split(" ").join("").replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
-    if (reversed === notReversed) {
+    let reverse = s.split(" ").join("").split("").reverse().join("").replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    let string = s.split(" ").join("").replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+    if (reverse === string) {
         return true;
     } else {
         return false;
     }
+
 };
+
+// to remove all non-alphanumeric characters: .replace(/[^a-zA-Z0-9]/g
