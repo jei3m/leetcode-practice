5bd23509d0b9719f8a5fa305f39edf4723a8df0e
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-31 06:06:06 +0800

    Time: 0 ms (100.00%) | Memory: 53 MB (87.10%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    // create an array from s
-    let sArr = s.trim().split(" ");
-    let arr = []
-    // remove blank values within s array
-    for (let i = 0; i < sArr.length; i++){
-        if (sArr[i] !== ""){
-            arr.push(sArr[i])
-        }
-    }
-    return arr[arr.length - 1].length;
+    string = s.trim().split(" ")
+    return string[string.length - 1].length
 };
