6f06241895214f8038cfd85ad6544d1cd1132444
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-31 05:40:04 +0800

    Time: 37 ms (80.67%) | Memory: 55.1 MB (6.34%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -3,7 +3,7 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
+var map = function (arr, fn) {
     let array = [];
     for (let i = 0; i < arr.length; i++) {
         let result = fn(arr[i], i);
