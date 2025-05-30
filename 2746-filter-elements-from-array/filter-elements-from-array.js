df11c5c35bb747ce1aaf7f7ef5709e60473c24cb
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-05-31 05:45:41 +0800

    Time: 47 ms (24.92%) | Memory: 53.2 MB (77.09%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,11 +4,15 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let array = [];
-    for (let i = 0; i < arr.length; i++){
+    let array = []
+    for (let i = 0; i < arr.length; i++) {
         if (fn(arr[i], i)) {
-            array.push(arr[i]);
+            array.push(arr[i])
         }
     }
     return array;
 };
+
+// start by declaring and empty array
+// loop over each element in the array
+// if fn(arr[i], i), push arr[i]
