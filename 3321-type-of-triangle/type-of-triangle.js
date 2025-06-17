f3130d550a6d78c7e67f69fd540bacd2baf2e067
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-06-17 23:53:24 +0800

    Time: 0 ms (100.00%) | Memory: 55.4 MB (31.95%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.js
+++ 3321-type-of-triangle/type-of-triangle.js
@@ -4,17 +4,15 @@
  */
 var triangleType = function(nums) {
     [a,b,c] = nums;
-    if (a + b > c && a + c > b && b + c > a) {
-
-        if (a === b && b === c){
+    const isTriangle = a + b > c && a + c > b && b + c > a;
+    if (isTriangle) {
+        if (a === b && b === c) {
             return "equilateral";
         } else if (a === b || b === c || a === c) {
             return "isosceles";
         } else {
             return "scalene";
         }
-
-    } else {
-        return "none";
     }
+    return "none";
 };
