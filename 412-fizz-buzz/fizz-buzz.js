395b41b0fd586acc6aa0c78a4cbb905ee09feb98
Justin Miguel Reyes <113350529+jei3m@users.noreply.github.com>
2025-06-18 00:02:28 +0800

    Time: 2 ms (40.59%) | Memory: 57.3 MB (72.55%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.js
+++ 412-fizz-buzz/fizz-buzz.js
@@ -3,17 +3,17 @@
  * @return {string[]}
  */
 var fizzBuzz = function(n) {
-    let arr = [];
+    let result = [];
     for (let i = 1; i <= n; i++) {
         if (i % 3 === 0 && i % 5 === 0) {
-            arr.push("FizzBuzz");
+            result.push("FizzBuzz");
         } else if (i % 3 === 0) {
-            arr.push("Fizz");
-        } else if (i % 5 === 0) {
-            arr.push("Buzz");
+            result.push("Fizz");
+        } else if (i % 5 === 0 ) {
+            result.push("Buzz");
         } else {
-            arr.push(i.toString())
+            result.push(i.toString());
         }
     }
-    return arr;
+    return result;
 };
